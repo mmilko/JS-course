7ba8d7590a05b60c8f54e962ea3bf32b10ade068
mmilko <i.mitkovski@gmail.com>
2017-07-11 22:16:20 +0300

    Update and rename next-day.js to nextday.js

--- 7. Next Day/nextday.js
+++ 7. Next Day/nextday.js
@@ -1,4 +1,4 @@
-function calcNextDay([year, month, day]) {
+function calcNextDay(year, month, day) {
     let date = new Date(year, month-1, day);
     let oneDay = 24 * 60 * 60 * 1000;
     let nextDate = new Date(date.getTime() + oneDay);
