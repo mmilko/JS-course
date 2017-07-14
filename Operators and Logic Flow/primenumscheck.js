1ed4ab0faadcc0292344a4c07052469acd7e2046
mmilko <i.mitkovski@gmail.com>
2017-07-15 02:08:25 +0300

    Update primenumscheck.js

--- Operators and Logic Flow/primenumscheck.js
+++ Operators and Logic Flow/primenumscheck.js
@@ -2,6 +2,7 @@ function isPrime(num) {
     let prime = true;
 
     for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num == i) continue;
         if (num % i == 0) {
             prime = false;
             break;
