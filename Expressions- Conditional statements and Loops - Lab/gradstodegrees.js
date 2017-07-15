76c8e46625bfa753f1a9ac44fb33dbceb6fbe6ff
mmilko <i.mitkovski@gmail.com>
2017-07-15 17:08:20 +0300

    Create gradstodegrees.js

---
+++ Expressions- Conditional statements and Loops - Lab/gradstodegrees.js
@@ -0,0 +1,9 @@
+function gradToDegree(grads) {
+    let degrees = grads * 3.6 / 4;
+    degrees = degrees % 360;
+
+    if (degrees < 0) {
+        degrees = 360 + degrees;
+    }
+    console.log(degrees);
+}
