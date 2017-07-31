7fd2e5b8884f6e4b69ff8250bc5f5b83efed159b
mmilko <i.mitkovski@gmail.com>
2017-07-31 23:17:36 +0300

    Create lastmonth.js

---
+++ Expressions- Conditional statements and Loops - Lab/lastmonth.js
@@ -0,0 +1,7 @@
+function lastMonth([day, month, year]) {
+    [day, month, year] = [day, month, year].map(Number);
+    let date = new Date(year, month-1, 0);
+    let days = date.getDate();
+
+    console.log(days);
+}
