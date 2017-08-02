a650fcda366bc36b430cc3f73ebb01d3aded314a
mmilko <i.mitkovski@gmail.com>
2017-08-02 22:35:04 +0300

    Create calculator.js

---
+++ Functions/calculator.js
@@ -0,0 +1,29 @@
+function calculate(a, b, op) {
+    function doIt(a, b, operation) {
+        return operation(a, b);
+    }
+
+    let add = function (a, b) {
+        return a + b;
+    };
+    let subtract = function (a, b) {
+        return a - b;
+    };
+    let multiply = function (a, b) {
+        return a * b;
+    };
+    let divide = function (a, b) {
+        return a / b;
+    };
+
+    switch (op) {
+        case '+':
+            return doIt(a, b, add);
+        case '-':
+            return doIt(a, b, subtract);
+        case '*':
+            return doIt(a, b, multiply);
+        case '/':
+            return doIt(a, b, divide);
+    }
+}
