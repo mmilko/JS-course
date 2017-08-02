17d96d374ac41f755548351b778da8fa08e306b3
mmilko <i.mitkovski@gmail.com>
2017-08-02 20:50:07 +0300

    Create squarestars.js

---
+++ Functions/squarestars.js
@@ -0,0 +1,10 @@
+function printSquare(count = 5) {
+
+    function printStars(n = count) {
+        console.log('*' + ' *'.repeat(n-1));
+    }
+
+    for (let i =1; i <= count; i++) {
+        printStars();
+    }
+}
